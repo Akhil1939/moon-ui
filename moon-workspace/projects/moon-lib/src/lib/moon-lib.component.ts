6f88317f055c554b326e5308afe0a79610947e3f
AKhil Kukadiya <akhilkukadiya464@gmail.com>
2024-06-08 12:23:46 +0530

    ui and lib setup

---
+++ moon-workspace/projects/moon-lib/src/lib/moon-lib.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-moon-lib',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      moon-lib works! hello
+    </p>
+  `,
+  styles: ``
+})
+export class MoonLibComponent {
+
+}
---
+++ moon-workspace/projects/moon-lib/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of moon-lib
+ */
+
+export * from './lib/moon-lib.service';
+export * from './lib/moon-lib.component';
